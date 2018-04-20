57a57e08b3da7d2b4ea65e3e9b2527740fde9db7
Adam Morgan <atom.morgan@gmail.com>
2018-04-20 17:24:33 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Remove Instagram Popup
+
+A Chrome extension to remove that annoying fucking popup to install the Instagram app.
+
---
+++ background-script.js
@@ -0,0 +1,4 @@
+﻿setTimeout(function() {
+  var element = document.querySelector('div._j8mem._6in6f');
+  element.parentNode.removeChild(element);
+}, 4000);
